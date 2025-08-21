4ade068cc1e99b645be4e3dd92fd991a03db7011
Oleksandr Kindirov <kindiole@cvut.cz>
2025-08-21 18:53:01 +0200

    Clean

--- src/main/java/cz/ami/cas/inauth/InalogyAuthenticatorBypassEvaluator.java
+++
@@ -1,9 +0,0 @@
-package cz.ami.cas.inauth;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@Documented
-public @interface InalogyAuthenticatorBypassEvaluator {
-}
